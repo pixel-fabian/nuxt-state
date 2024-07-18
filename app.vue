97eee163689588d51983f08fc2c5bf6774873817
pixel-fabian <82926714+pixel-fabian@users.noreply.github.com>
2024-07-18 21:47:30 +0200

    feat: :sparkles: ADD global nav component

--- app.vue
+++ app.vue
@@ -1,15 +1,11 @@
 <script setup lang="ts">
+import Navigation from './global/components/Navigation.vue';
+
 </script>
 
 <template>
   <div>
-    <div class="links">
-      <NuxtLink to="/">Home</NuxtLink>
-      <NuxtLink to="/app-1/">App1-Index</NuxtLink>
-      <NuxtLink to="/app-1/page2">App1-Page2</NuxtLink>
-      <NuxtLink to="/app-2/">App2-Index</NuxtLink>
-      <NuxtLink to="/app-2/page2">App2-Page2</NuxtLink>
-    </div>
+<Navigation></Navigation>
     <NuxtPage />
   </div>
 
---
+++ global/components/Navigation.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="links">
+      <NuxtLink to="/">Home</NuxtLink>
+      <NuxtLink to="/app-1/">App1-Index</NuxtLink>
+      <NuxtLink to="/app-1/page2">App1-Page2</NuxtLink>
+      <NuxtLink to="/app-2/">App2-Index</NuxtLink>
+      <NuxtLink to="/app-2/page2">App2-Page2</NuxtLink>
+    </div>
+</template>
+
+<style lang="css" scoped>
+.links {
+  width: 600px;
+  display: flex;
+  justify-content: space-between;
+}
+</style>
